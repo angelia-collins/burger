5e5b6ead7f4b5acd65d05f5821740d19561859fd
angelia collins <angiecollins09@gmail.com>
2021-03-27 13:15:12 -0500

    everything is burger now

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,6 @@
-// Import MySQL connection.
 var connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -63,11 +58,9 @@ var orm = {
     console.log(queryString);
 
     connection.query(queryString, vals, function(err, result) {
-      if (err) {
+      if (err)
         throw err;
-      }
-
-      cb(result);
+        cb(result);
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
@@ -81,11 +74,9 @@ var orm = {
 
     console.log(queryString);
     connection.query(queryString, function(err, result) {
-      if (err) {
+      if (err) 
         throw err;
-      }
-
-      cb(result);
+        cb(result);      
     });
   },
   delete: function(table, condition, cb) {
@@ -94,10 +85,8 @@ var orm = {
     queryString += condition;
 
     connection.query(queryString, function(err, result) {
-      if (err) {
+      if (err) 
         throw err;
-      }
-
       cb(result);
     });
   }
